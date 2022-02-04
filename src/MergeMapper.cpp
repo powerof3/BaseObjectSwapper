8282529c9e77301342c6cb906ba67629531eda02
Alan Tse <alandtse@gmail.com>
2022-02-03 20:58:16 -0800

    Normalize mappings
    Fixes a bug where different case or formats for numbers would result in
    a missed match. Now all strings are lower case and numbers are stored as
    normalized hex strings.

--- src/MergeMapper.cpp
+++ src/MergeMapper.cpp
@@ -8,6 +8,7 @@ namespace MergeMapper
 		logger::info("Searching for merges within the Data folder");
 		auto constexpr folder = R"(Data\)";
 		json json_data;
+		auto total = 0;
 		for (const auto& entry : std::filesystem::directory_iterator(folder)) {
 			// zMerge folders have name "merge - 018auri"
 			auto constexpr mergePrefix = R"(Data\merge - )";
@@ -25,11 +26,24 @@ namespace MergeMapper
 				}
 				if (!json_data.empty()) {
 					for (auto& [esp, idmap] : json_data.items()) {
-						logger::debug(" Found {} maps to {} with {} mappings", esp, merged, idmap.size());
-						if (mergeMap.contains(esp))
+						auto espkey = esp;
+						std::transform(espkey.begin(), espkey.end(), espkey.begin(), ::tolower);
+						if (idmap.size()) {
+							logger::info(" Found {} maps to {} with {} mappings", esp, merged, idmap.size());
+							total += idmap.size();
+						}
+						if (mergeMap.contains(espkey))
 							logger::warn(" Duplicate {} found in {}", esp, merged);
-						mergeMap[esp]["name"] = merged;
-						mergeMap[esp]["map"] = idmap;
+						if (!idmap.empty()) {
+							for (auto& [key, value] : idmap.items()){
+								auto storedKey = std::to_string(std::stoi(key, 0, 16));
+								std::transform(storedKey.begin(), storedKey.end(), storedKey.begin(), ::tolower);
+								auto storedValue = std::to_string(std::stoi(value.get<std::string>(), 0, 16));
+								std::transform(storedValue.begin(), storedValue.end(), storedValue.begin(), ::tolower);
+								mergeMap[espkey]["map"][storedKey] = storedValue;
+							}
+							mergeMap[espkey]["name"] = merged;
+						}
 					}
 				}
 			}
@@ -38,20 +52,25 @@ namespace MergeMapper
 			logger::info("	No merges were found within the Data folder");
 			return false;
 		}
-		logger::info("	{} merges found", mergeMap.size());
+		logger::info("	{} merges found with {} mappings", mergeMap.size(), total);
 		return true;
 	}
 
 	std::pair<std::string, RE::FormID> GetNewFormID(std::string oldName, std::string oldFormID)
 	{
-		std::string modName = oldName;
+		auto modName = oldName;
+		auto espkey = oldName;
+		std::transform(espkey.begin(), espkey.end(), espkey.begin(), ::tolower);
 		RE::FormID formID = std::stoi(oldFormID, 0, 16);
 		//check for merged esps
-		if (mergeMap.contains(oldName)) {
-			modName = mergeMap[oldName]["name"];
-			if (!mergeMap[oldName]["map"].empty()) {
-				if (mergeMap[oldName]["map"].contains(oldFormID)) {
-					formID = std::stoi(mergeMap[oldName]["map"][oldFormID].get<std::string>());
+		if (mergeMap.contains(espkey)) {
+			modName = mergeMap[espkey]["name"];
+			mergeMap[espkey]["map"];
+			auto storedKey = std::to_string(formID);
+			if (!mergeMap[espkey]["map"].empty()) {
+				std::transform(storedKey.begin(), storedKey.end(), storedKey.begin(), ::tolower);
+				if (mergeMap[espkey]["map"].contains(storedKey)) {
+					formID = std::stoi(mergeMap[espkey]["map"][storedKey].get<std::string>(), 0, 16);
 				}
 			}
 		}
